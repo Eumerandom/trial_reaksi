f10b2d177d8e3980e45d23aca1f5a7d326822941
dhaniil <pemudakrinj20394@gmail.com>
2025-04-05 15:05:45 +0700

    [Update] Kecer

---
+++ app/Models/Company.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Company extends Model
+{
+    use SoftDeletes;
+    protected $table = 'companies';
+    protected $fillable = ['name', 'slug', 'parent_id', 'status', 'logo'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Company::class, 'parent_id', 'id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Company::class, 'parent_id', 'id');
+    }
+
+}
