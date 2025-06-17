7456f67260730669097b149fcd300a10bdc3ccaa
HandlerOne <pemudakrinj20394@gmail.com>
2025-06-17 18:10:26 +0700

    [Update] Tailwindcss Package to v4

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+        }),
+        tailwindcss({
+            content: [
+                './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+                './vendor/laravel/jetstream/**/*.blade.php',
+                './storage/framework/views/*.php',
+                './resources/views/**/*.blade.php',
+                './resources/**/*.blade.php',
+                './resources/**/*.js',
+                './resources/**/*.vue',
+            ],
         }),
     ],
 });
