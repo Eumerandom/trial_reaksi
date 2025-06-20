e7f0e1ddade982e07ece5443919be3e8ebee097a
HandlerOne <pemudakrinj20394@gmail.com>
2025-06-20 18:01:48 +0700

    [Fix] Tailwindcss v4 changes theme

--- vite.config.js
+++ vite.config.js
@@ -8,18 +8,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss({
-            content: [
-                './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-                './vendor/laravel/jetstream/**/*.blade.php',
-                './storage/framework/views/*.php',
-                './resources/views/**/*.php',
-                './resources/views/**/*.blade.php',
-                './resources/views/errors/*.blade.php',
-                './resources/**/*.blade.php',
-                './resources/**/*.js',
-                './resources/**/*.vue',
-            ],
-        }),
+        tailwindcss(),
     ],
 });
