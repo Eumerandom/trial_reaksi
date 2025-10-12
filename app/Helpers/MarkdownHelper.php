7dbce17d55188a89a1822c9ac783a4dc2f58c14f
LazTech <laznology.dev@gmail.com>
2025-10-12 15:18:52 +0700

    feat(helper): create helper function for ToC processing

---
+++ app/Helpers/MarkdownHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Str;
+use Spatie\LaravelMarkdown\MarkdownRenderer;
+
+/**
+ * @param string $markdown
+ * @return string
+ */
+function renderWithAnchor(string $markdown): string
+{
+    $html = app(MarkdownRenderer::class)->toHtml($markdown);
+    return preg_replace_callback('/<h([1-6])>(.*?)<\/h\1>/', function ($matches) {
+        $level = $matches[1];
+        $text = strip_tags($matches[2]);
+        $id = Str::slug($text);
+        return "<h{$level} id=\"{$id}\">{$matches[2]}</h{$level}>";
+    }, $html);
+}
+
+/**
+ * @param string $markdown
+ * @return array
+ */
+function extractToc(string $markdown): array
+{
+    $headings = [];
+    $lines = preg_split('/\r\n|\r|\n/', $markdown);
+
+    foreach ($lines as $line) {
+        if (preg_match('/^(#{1,6})\s+(.*)$/', trim($line), $matches)) {
+            $level = strlen($matches[1]);
+            $text = $matches[2];
+            $id = Str::slug($text);
+
+            $headings[] = [
+                'level' => (int)$level,
+                'text' => $text,
+                'id' => $id,
+            ];
+        }
+    }
+    return $headings;
+}
